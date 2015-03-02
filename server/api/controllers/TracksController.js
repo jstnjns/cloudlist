329c6d0883f0180aec004dfaf8e66a97c1a192a1
Justin Jones <justin@jstnjns.com>
2015-03-01 17:05:12 -0800

    Starting tracks

---
+++ server/api/controllers/TracksController.js
@@ -0,0 +1,11 @@
+/**
+ * TracksController
+ *
+ * @description :: Server-side logic for managing tracks
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
